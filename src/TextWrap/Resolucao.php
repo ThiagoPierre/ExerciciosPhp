62d30ab6cea4de5d91e17ba2f8c0b42303edb266
ThiagoPierre <69355135+ThiagoPierre@users.noreply.github.com>
2020-11-05 15:15:26 -0300

    Update Resolucao.php

--- src/TextWrap/Resolucao.php
+++ src/TextWrap/Resolucao.php
@@ -1,40 +1,33 @@
 <?php
 /**
- * Transforma acento em palavras normais
+ * Função algoritmica para simular a função wordwrap
  * 
- * @param string $str
- *    string a ser utilizada para retirada de acentos
- * @return string
- *    retorna uma string sem acentos.
+ * @file 
  */
-function stripAccents($str)
-{
-    return strtr(utf8_decode($str), utf8_decode('àáâãäçèéêëìíîïñòóôõöùúûüýÿÀÁÂÃÄÇÈÉÊËÌÍÎÏÑÒÓÔÕÖÙÚÛÜÝ'), 'aaaaaceeeeiiiinooooouuuuyyAAAAACEEEEIIIINOOOOOUUUUY');
-}
+
 /**  
  * Quebra uma string em diversas strings com tamanho definido.
  * 
- * @param  string $text
- *    Texto utilizado como entrada
- * @param  int $length
- *    Máximo de caracteres por linha
+ * @param string $text   Texto utilizado como entrada
+ * @param int    $length Máximo de caracteres por linha
+ * 
  * @return array
  * array com as strings já cortadas pelo limite de caracteres
  */
 function textWrap(string $text, int $length)
 {
-    $pedaco = explode(" ", stripAccents($text));
+    $pedaco = explode(" ", $text);
     $result = array();
     $final_result = array();
     $lengthcounter = 0;
     $save_word = "";
     $w = 0;
     for ($x = 0; $x < sizeof($pedaco); $x++) {
-        if ($w + strlen($pedaco[$x]) <= $length) {
+        if ($w + mb_strlen($pedaco[$x]) <= $length) {
             array_push($result, $pedaco[$x]);
-            $w +=  strlen($pedaco[$x]);
+            $w +=  mb_strlen($pedaco[$x]);
         } else {
-            $width = strlen($pedaco[$x]) / $length;
+            $width = mb_strlen($pedaco[$x]) / $length;
             $count = 0;
             while ($count < $width) {
                 array_push($result, substr($pedaco[$x], $length * $count, $length));
@@ -43,7 +36,7 @@ function textWrap(string $text, int $length)
         }
     }
     for ($i = 0; $i < sizeof($result); $i++) {
-        $lengthcounter += strlen($result[$i]);
+        $lengthcounter += mb_strlen($result[$i]);
         if ($lengthcounter <= $length) {
             $save_word .= $result[$i] . " ";
             $lengthcounter++;
