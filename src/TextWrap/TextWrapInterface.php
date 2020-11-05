31c185e63cc86131e9f835f06d1bd15ce37e7282
ThiagoPierre <69355135+ThiagoPierre@users.noreply.github.com>
2020-11-05 17:04:52 -0300

    Update TextWrapInterface.php

--- src/TextWrap/TextWrapInterface.php
+++ src/TextWrap/TextWrapInterface.php
@@ -6,6 +6,17 @@ namespace Galoa\ExerciciosPhp\TextWrap;
  * Define uma interface para o exercício de quebra de linha.
  */
 interface TextWrapInterface {
+  /**
+   * Paramêtros da função.
+   *
+   * @param $text
+   *   Frase ou texto a ser analisado
+   * @param $length
+   *   Limite de caracteres por linha
+   *
+   * @return array
+   */
+  
   public function textWrap(string $text, int $length): array;
 
 }
