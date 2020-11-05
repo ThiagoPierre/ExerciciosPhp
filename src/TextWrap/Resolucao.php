f182649538391ecfc1be9c79b73a5af99da9ed6f
ThiagoPierre <69355135+ThiagoPierre@users.noreply.github.com>
2020-11-05 14:56:18 -0300

    Update Resolucao.php

--- src/TextWrap/Resolucao.php
+++ src/TextWrap/Resolucao.php
@@ -1,43 +1,59 @@
 <?php
-
-namespace Galoa\ExerciciosPhp\TextWrap;
-
-class Resolucao implements TextWrapInterface
+/**
+ * Transforma acento em palavras normais
+ * 
+ * @param string $str
+ *    string a ser utilizada para retirada de acentos
+ * @return string
+ *    retorna uma string sem acentos.
+ */
+function stripAccents($str)
 {
-    public function textWrap(string $text, int $length): array
-    {
-        $pedaco = explode(" ", $text);
-        $result = array();
-        $w = 0;
-        $final_result = array();
-        $c = 0;
-        $a = "";
-        for ($x = 0; $x < sizeof($pedaco); $x++) {
-            if ($w + strlen($pedaco[$x]) <= $length) {
-                array_push($result, $pedaco[$x]);
-                $w +=  strlen($pedaco[$x]);
-            } else {
-                $width = strlen($pedaco[$x]) / $length;
-                $count = 0;
-                while ($count < $width) {
-                    array_push($result, substr($pedaco[$x], $length * $count, $length));
-                    $count++;
-                }
+    return strtr(utf8_decode($str), utf8_decode('àáâãäçèéêëìíîïñòóôõöùúûüýÿÀÁÂÃÄÇÈÉÊËÌÍÎÏÑÒÓÔÕÖÙÚÛÜÝ'), 'aaaaaceeeeiiiinooooouuuuyyAAAAACEEEEIIIINOOOOOUUUUY');
+}
+/**  
+ * Quebra uma string em diversas strings com tamanho definido.
+ * 
+ * @param  string $text
+ *    Texto utilizado como entrada
+ * @param  int $length
+ *    Máximo de caracteres por linha
+ * @return array
+ * array com as strings já cortadas pelo limite de caracteres
+ */
+function textWrap(string $text, int $length)
+{
+    $pedaco = explode(" ", stripAccents($text));
+    $result = array();
+    $final_result = array();
+    $lengthcounter = 0;
+    $save_word = "";
+    $w = 0;
+    for ($x = 0; $x < sizeof($pedaco); $x++) {
+        if ($w + strlen($pedaco[$x]) <= $length) {
+            array_push($result, $pedaco[$x]);
+            $w +=  strlen($pedaco[$x]);
+        } else {
+            $width = strlen($pedaco[$x]) / $length;
+            $count = 0;
+            while ($count < $width) {
+                array_push($result, substr($pedaco[$x], $length * $count, $length));
+                $count++;
             }
         }
-        for ($i = 0; $i < sizeof($result); $i++) {
-            $c += strlen($result[$i]);
-            if ($c <= $length) {
-                $a .= " " . $result[$i];
-                $c++;
-            } else {
-                array_push($final_result, trim($a));
-                $i--;
-                $c = 0;
-                $a = "";
-            }
-        }
-        array_push($final_result, trim($a));
-        return $final_result;
     }
+    for ($i = 0; $i < sizeof($result); $i++) {
+        $lengthcounter += strlen($result[$i]);
+        if ($lengthcounter <= $length) {
+            $save_word .= $result[$i] . " ";
+            $lengthcounter++;
+        } else {
+            array_push($final_result, $save_word);
+            $i--;
+            $lengthcounter = 0;
+            $save_word = "";
+        }
+    }
+    array_push($final_result, trim($save_word));
+    return $final_result;
 }
