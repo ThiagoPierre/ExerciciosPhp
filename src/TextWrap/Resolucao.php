5db71fdbb0962dbbb4f28a412b5db9d4d077db11
ThiagoPierre <69355135+ThiagoPierre@users.noreply.github.com>
2020-11-05 15:32:43 -0300

    Update Resolucao.php

--- src/TextWrap/Resolucao.php
+++ src/TextWrap/Resolucao.php
@@ -1,52 +1,42 @@
 <?php
-/**
- * Função algoritmica para simular a função wordwrap
- * 
- * @file 
- */
+namespace Galoa\ExerciciosPhp\TextWrap;
 
-/**  
- * Quebra uma string em diversas strings com tamanho definido.
- * 
- * @param string $text   Texto utilizado como entrada
- * @param int    $length Máximo de caracteres por linha
- * 
- * @return array
- * array com as strings já cortadas pelo limite de caracteres
- */
-function textWrap(string $text, int $length)
+class Resolucao implements TextWrapInterface
 {
-    $pedaco = explode(" ", $text);
-    $result = array();
-    $final_result = array();
-    $lengthcounter = 0;
-    $save_word = "";
-    $w = 0;
-    for ($x = 0; $x < sizeof($pedaco); $x++) {
-        if ($w + mb_strlen($pedaco[$x]) <= $length) {
-            array_push($result, $pedaco[$x]);
-            $w +=  mb_strlen($pedaco[$x]);
-        } else {
-            $width = mb_strlen($pedaco[$x]) / $length;
-            $count = 0;
-            while ($count < $width) {
-                array_push($result, substr($pedaco[$x], $length * $count, $length));
-                $count++;
+    public function textWrap(string $text, int $length): array
+    {
+        $pedaco = explode(" ", $text);
+        $result = array();
+        $final_result = array();
+        $lengthcounter = 0;
+        $save_word = "";
+        $w = 0;
+        for ($x = 0; $x < sizeof($pedaco); $x++) {
+            if ($w + mb_strlen($pedaco[$x]) <= $length) {
+                array_push($result, $pedaco[$x]);
+                $w +=  mb_strlen($pedaco[$x]);
+            } else {
+                $width = mb_strlen($pedaco[$x]) / $length;
+                $count = 0;
+                while ($count < $width) {
+                    array_push($result, substr($pedaco[$x], $length * $count, $length));
+                    $count++;
+                }
             }
         }
-    }
-    for ($i = 0; $i < sizeof($result); $i++) {
-        $lengthcounter += mb_strlen($result[$i]);
-        if ($lengthcounter <= $length) {
-            $save_word .= $result[$i] . " ";
-            $lengthcounter++;
-        } else {
-            array_push($final_result, $save_word);
-            $i--;
-            $lengthcounter = 0;
-            $save_word = "";
+        for ($i = 0; $i < sizeof($result); $i++) {
+            $lengthcounter += mb_strlen($result[$i]);
+            if ($lengthcounter <= $length) {
+                $save_word .= $result[$i] . " ";
+                $lengthcounter++;
+            } else {
+                array_push($final_result, $save_word);
+                $i--;
+                $lengthcounter = 0;
+                $save_word = "";
+            }
         }
+        array_push($final_result, trim($save_word));
+        return $final_result;
     }
-    array_push($final_result, trim($save_word));
-    return $final_result;
 }
